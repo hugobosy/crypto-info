b1692664381cbc960a2a6c4b02ea9a5bb4a09108
HugoBosy <hkliszcz@gmail.com>
2022-06-06 13:44:18 +0200

    download data from binance - add Data.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {Data} from "../data/Data";
 
 function App() {
+
+  const data = Data();
+
+
   return (
     <div className="App">
-
+      <select>
+        {
+          data
+              .map(pair => <option value={pair.symbol} key={pair.symbol}>{pair.symbol}</option>)
+        }
+      </select>
     </div>
   );
 }
